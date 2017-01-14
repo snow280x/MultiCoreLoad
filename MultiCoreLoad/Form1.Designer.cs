759a5030bf6d678b565318f78bb193e25e703463
snow280x <sal.snow3510@gmail.com>
2017-01-15 00:27:09 +0900

    double buffered

--- MultiCoreLoad/Form1.Designer.cs
+++ MultiCoreLoad/Form1.Designer.cs
@@ -79,6 +79,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(50, 50);
+            this.DoubleBuffered = true;
             this.Font = new System.Drawing.Font("Yu Gothic UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.ForeColor = System.Drawing.Color.White;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
