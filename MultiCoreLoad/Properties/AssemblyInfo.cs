d258ff450ec602a178ced025a74a7178d203e36e
snow280x <sal.snow3510@gmail.com>
2017-04-12 06:40:34 +0900

    update information

--- MultiCoreLoad/Properties/AssemblyInfo.cs
+++ MultiCoreLoad/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MultiCoreLoad")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © snow280x 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
