4d6d3552e543934ebb8ffaa594f4038a86695369
snow280x <sal.snow3510@gmail.com>
2017-10-02 03:47:15 +0900

    improve

--- MultiCoreLoad/Form1.cs
+++ MultiCoreLoad/Form1.cs
@@ -134,7 +134,7 @@ namespace MultiCoreLoad
             }
             last = now;
 
-            if (error <= 50)
+            if (error <= 10)
             {
                 if (Process.GetCurrentProcess().PriorityClass != ProcessPriorityClass.Idle)
                 {
